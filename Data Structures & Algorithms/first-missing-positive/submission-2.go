9f966a979eda3a2c39280fc0c7159e5ad7594554
Cameron Scott McCreery <cameron@mccreery.dev>
2026-07-03 16:37:48 +0000

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.go
@@ -0,0 +1,30 @@
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	i := 0
+
+
+	for i < n {
+		// If the value at i is less than 0 or > n, skip
+		if nums[i] <= 0 || nums[i] > n {
+			i++
+			continue
+		}
+
+		index := nums[i] - 1
+		// if nums[i] and nums[index] don't match. swap them, otherwise continue
+		if nums[i] != nums[index] {
+			nums[i], nums[index] = nums[index], nums[i]
+		} else {
+			i++
+		}
+	}
+
+	// Sweep through the now 'sorted' array and find the value that doesn't satisfy the constraint
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i+1
+		}
+	}
+
+	return n+1
+}

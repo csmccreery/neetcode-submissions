6f2421136f4e751ec119fe51d62f2908be91e072
Cameron Scott McCreery <cameron@mccreery.dev>
2026-07-16 18:01:53 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (um.find(complement) != um.end()) {
+                return {um[complement], i};
+            }
+
+            um[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};

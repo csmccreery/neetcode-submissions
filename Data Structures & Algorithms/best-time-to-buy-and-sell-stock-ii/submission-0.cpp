c0bd3bef14b29ce8c0483ce3e26df7fc29c33f9e
Cameron Scott McCreery <cameron@mccreery.dev>
2026-07-23 16:33:42 +0000

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        bool holding_share = false;
+
+        for (int i = 0; i < prices.size(); i++) {
+            int today = prices[i];
+            int tomorrow = (i + 1) >= prices.size() ? 
+                prices[i] : prices[i + 1];
+
+            bool buy = today < tomorrow; 
+            bool sell = today >= tomorrow;
+            if (buy && !holding_share) {
+               profit -= prices[i];
+               holding_share = true; 
+            }
+
+            if (sell && holding_share) {
+                profit += prices[i];
+                holding_share = false;
+            }
+        }
+        
+        return profit;
+    }
+};

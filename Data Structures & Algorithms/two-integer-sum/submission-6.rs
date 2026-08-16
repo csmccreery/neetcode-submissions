916006952fc6f339c1fedff3d417cffde61ef264
Cameron Scott McCreery <cameron@mccreery.dev>
2026-08-16 17:37:52 +0000

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let n: usize = nums.len();
+        let mut seen: HashMap<i32, usize> = HashMap::new();        
+
+        for i in 0..n {
+            match seen.get(&(target - nums[i])) {
+                Some(&j) => { return vec![j as i32, i as i32] },
+                None => { seen.insert(nums[i], i); },
+            }
+        }
+
+        return vec![-1, -1];
+    }
+}
